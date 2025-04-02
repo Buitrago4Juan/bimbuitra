fa9108ea41e88d4e3851cccd1fda2941878f255c
Buitrago Juan <juanebuitragof4@gmail.com>
2025-04-02 08:29:43 -0500

    Arreglo codigo

--- carpeta_06/main.py
+++ carpeta_06/main.py
@@ -1,6 +1,4 @@
-# Estudiante: corrige este código y haz un pull request con la versión corregida.
-
 try:
     x = int(input('Edad: '))
 except:
-print('Error en la entrada')
+    print('Error en la entrada')
